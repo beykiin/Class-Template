ae6772d00078de9c57d9fa2d50bdc5832db91030
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:27:42 +0300

    Add files via upload

---
+++ Class Template.cpp
@@ -0,0 +1,106 @@
+﻿// Sınıf Şablonu.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+//template<typename T>
+//
+//class Mat {
+//    public:
+//        T a{};
+//        T b{};
+//
+//        T topla() {
+//            return a + b;
+//        }
+//};
+//
+//
+//
+//int main()
+//{
+//    Mat<int> mat;
+//    mat.a = 7;
+//    mat.b = 1;
+//    mat.topla();
+//
+//    std::cout << mat.topla();
+//
+//    Mat<float> mat1;
+//    mat1.a = 7.2;
+//    mat1.b = 1.8;
+//    mat1.topla();
+//
+//    std::cout << mat1.topla();
+//
+//    Mat<double> mat2;
+//    mat2.a = 0.2587;
+//    mat2.b = 0.8523;
+//    mat2.topla();
+//
+//    std::cout << mat2.topla();
+//}
+
+
+//class Silah{};
+//class Mermi{};
+//
+//template<class T>
+//T* nesneYap() {
+//
+//    T nesne;
+//    return &nesne;
+//}
+//
+//int main()
+//{
+//
+//    std::cout << nesneYap<Silah>();
+//
+//
+//}
+
+//class Silah {};
+//class Mermi {};
+//
+//template<typename T>
+//T* nesneYap(T nesne) {
+//    return &nesne;
+//}
+//
+//int main()
+//{
+//    Silah silah;
+//
+//    std::cout << nesneYap<Silah>(silah);
+//
+//
+//}
+
+//class Silah {};
+//class Mermi {};
+//
+//template<typename T>
+//T* nesneYap(T* nesne) {
+//    return nesne;
+//}
+//
+//int main()
+//{
+//    Silah silah;
+//
+//    std::cout << nesneYap<Silah>(&silah);
+//
+//
+//}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
